453c4d8d1f89a9a010190c2820aff3730d7e38c6
Santiago Lamora <santiago@ribaguifi.com>
2022-11-08 13:53:52 +0100

    Define Greeting repr

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+    
+    def __str__(self):
+        return f"Greeting {self.when}"
+
